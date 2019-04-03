1fb841e8441a4e48ceccf6bd62118012807b1a49
Saad Baradan <saadbaradan@gmail.com>
2019-04-03 15:04:50 -0600

    Initial commit
    Co-authored-by: Duy Vu<duyv.thanh@gmail.com>
    Co-authored-by: Lynne Rang<lynne.rang@gmail.com>
    Co-authored-by: Joe Johnson<johnson.joseph527@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { breweries, beers } from './data.js';
+import Locations from './Locations.js';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      beers: beers,
+      breweries: breweries,
+      locations: []
+    }
+
+    this.getStates=this.getStates.bind(this)
+
+  }
+
+  componentDidMount() {
+    this.getStates()
+  }
+
+  getStates = () => {
+    let states = this.state.breweries.reduce((acc, currentBrewery) => {
+        if(!acc.includes(currentBrewery.state)) {
+          acc.push(currentBrewery.state)
+        }
+      return acc;
+    }, [])
+    this.setState({locations: states});
+  }
+
+
   render() {
+    console.log(this.state.locations)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Locations location={this.state.locations}/>
+        {/*<button type="button" onClick={this.assignStates}></button>*/}
       </div>
     );
   }
---
+++ src/Locations.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Locations extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    }
+
+
+
+
+  }
+
+
+
+
+
+
+}
