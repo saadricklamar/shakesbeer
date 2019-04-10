081ade9653a9f3f44b352b00307b9c2710fe980f
Lynne Rang <lynne.rang@gmail.com>
2019-04-10 16:07:19 -0600

    Fix bug that showed error when brewery had no beers by adding conditions for no matches being null and matches rendering.

--- src/Breweries.js
+++ src/Breweries.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import Beer from "./Beer.js"
+import Beer from './Beer.js'
 import './Breweries.scss';
-
 
 class Breweries extends Component {
   constructor(props) {
@@ -15,55 +14,55 @@ class Breweries extends Component {
     this.toggleFavorite = this.toggleFavorite.bind(this);
   }
 
-    getTarget = (e) => {
-      this.setState({breweryName: e.target.innerText}, () => {
-        this.findBeers(); 
-      }) 
-      this.toggleDropDown();
-    }
+  getTarget = (e) => {
+    this.setState({breweryName: e.target.innerText}, () => {
+      this.findBeers(); 
+    }) 
+    this.toggleDropDown();
+  }
 
-    findBeers = () => {
-      let newBeers = this.props.dataset.find(brewery => {
-        return this.state.breweryName === brewery.name;
-      }).beers
-      this.setState({beerList: newBeers});
-    }
+  findBeers = () => {
+    let match = this.props.dataset.find(brewery => {
+      return this.state.breweryName === brewery.name;
+    });
+    let beers = match && match.beers ? match.beers : null;
+    this.setState({beerList: beers});
+  }
 
-    toggleDropDown = () => {
-      this.setState({dropDown: !this.state.dropDown})
-    }
+  toggleDropDown = () => {
+    this.setState({dropDown: !this.state.dropDown})
+  }
 
-    toggleFavorite = () => {
-        !this.props.starredBreweries.includes(this.props.name) 
-        ? this.props.updateStarredList(this.state.breweryName, 'add')
-        : this.props.updateStarredList(this.state.breweryName)
-    }
-    
-    render() {
-      // let favClass = this.state.favorite ? 'fas far fa-star' : 'far fa-star'
-      let favClass = this.props.starredBreweries.includes(this.props.name) 
-      ? 'far fa-star fas' 
-      : 'far fa-star ';
+  toggleFavorite = () => {
+      !this.props.starredBreweries.includes(this.props.name) 
+      ? this.props.updateStarredList(this.state.breweryName, 'add')
+      : this.props.updateStarredList(this.state.breweryName)
+  }
+  
+  render() {
+    let favClass = this.props.starredBreweries.includes(this.props.name) 
+    ? 'far fa-star fas' 
+    : 'far fa-star ';
 
-      return (
-        <div className='brewery-label'>
-          <div className='brewery-header'>
-            <i className={favClass} onClick={this.toggleFavorite}></i>
-            <h2 onClick={this.getTarget}>{this.props.name}</h2>
-          </div>
-          {
-          this.state.dropDown ? (
-              this.state.beerList.map(beer => {
-                  return <Beer beers={this.state.beerList}
-                               beerName={beer.name}
-                               key={beer.FIELD1}
-                          />
-              })
-          ) : (null)
-          }
+    return (
+      <div className='brewery-label'>
+        <div className='brewery-header'>
+          <i className={favClass} onClick={this.toggleFavorite}></i>
+          <h2 onClick={this.getTarget}>{this.props.name}</h2>
         </div>
-      );
-    }
+        {
+        this.state.dropDown && this.state.beerList ? (
+            this.state.beerList.map(beer => {
+                return <Beer beers={this.state.beerList}
+                              beerName={beer.name}
+                              key={beer.FIELD1}
+                        />
+            })
+        ) : (null)
+        }
+      </div>
+    );
+  }
 }
 
 export default Breweries;
