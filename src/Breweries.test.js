8ac14137d2478fb966a3c1212e333604181e6cae
Joe Johnson <johnson.joseph527@gmail.com>
2019-05-22 11:58:52 -0600

    Updated tests in autocomplete, resultsPage, and breweries

--- src/Breweries.test.js
+++ src/Breweries.test.js
@@ -72,62 +72,15 @@ describe('Breweries', () => {
     });
 
     it('Should have default state', () => {
-        expect(wrapper.state()).toEqual({beerList: []})
+        expect(wrapper.state()).toEqual({
+            beerList: [
+                {"FIELD1": 43, "abv": 0.045, "brewery_id": 60, "ibu": 18, "id": 2544, "name": "Send Help", "ounces": 12, "style": "American Blonde Ale"},
+                {"FIELD1": 44, "abv": 0.055, "brewery_id": 60, "ibu": null, "id": 2324, "name": "Cast Iron Oatmeal Brown", "ounces": 12, "style": "American Brown Ale"},
+                {"FIELD1": 45, "abv": 0.06, "brewery_id": 60, "ibu": null, "id": 2288, "name": "Reprise Centennial Red", "ounces": 12, "style": "American Amber / Red Ale"},
+                {"FIELD1": 46, "abv": 0.055, "brewery_id": 60, "ibu": null, "id": 2287, "name": "Alter Ego", "ounces": 12, "style": "American Black Ale"}
+            ],
+            breweryName: "The Lion Brewery"
+            })
     });
-
-    // it('Should find the beer and setState', () => {
-    //     wrapper.state().breweryName = "The Lion Brewery"
-    //     expect(wrapper.state('beerList')).toEqual([])
-    //     wrapper.instance().findBeers()
-    //     expect(wrapper.state('beerList')).toEqual([ {
-    //         "FIELD1": 43,
-    //         "abv": 0.045,
-    //         "ibu": 18,
-    //         "id": 2544,
-    //         "name": "Send Help",
-    //         "style": "American Blonde Ale",
-    //         "brewery_id": 60,
-    //         "ounces": 12
-    //       },
-    //       {
-    //         "FIELD1": 44,
-    //         "abv": 0.055,
-    //         "ibu": null,
-    //         "id": 2324,
-    //         "name": "Cast Iron Oatmeal Brown",
-    //         "style": "American Brown Ale",
-    //         "brewery_id": 60,
-    //         "ounces": 12
-    //       },
-    //       {
-    //         "FIELD1": 45,
-    //         "abv": 0.06,
-    //         "ibu": null,
-    //         "id": 2288,
-    //         "name": "Reprise Centennial Red",
-    //         "style": "American Amber / Red Ale",
-    //         "brewery_id": 60,
-    //         "ounces": 12
-    //       },
-    //       {
-    //         "FIELD1": 46,
-    //         "abv": 0.055,
-    //         "ibu": null,
-    //         "id": 2287,
-    //         "name": "Alter Ego",
-    //         "style": "American Black Ale",
-    //         "brewery_id": 60,
-    //         "ounces": 12
-    //       }
-    //     ])
-    // });
-
-    // it('Should setState of breweryName to the target given', () => {
-    //     const instance = wrapper.instance()
-    //     const mockTarget = {target:{innerText: "The Lion Brewery"}}
-    //     expect(wrapper.state('breweryName')).toEqual('')
-    //     instance.getTarget(mockTarget)
-    //     expect(wrapper.state('breweryName')).toEqual("The Lion Brewery")
-    // });
 });
 
